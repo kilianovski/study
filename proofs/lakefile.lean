6bb6a8388686b4c281f896b9288c6cd71aae6033
mi sha kilianovski <kilianovski@gmail.com>
2024-03-10 08:03:00 +0100

    Init `my_lean` package

---
+++ proofs/lakefile.lean
@@ -0,0 +1,9 @@
+import Lake
+open Lake DSL
+
+package my_lean {
+  -- add package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "d8c7d9897527e99b28862cdf5a4a13aefee95897"
